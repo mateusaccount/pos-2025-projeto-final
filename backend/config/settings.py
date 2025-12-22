dcec62f42640789a4251ba6cbea2eecb033c17a8
João Henrique <johenriqueos2007@gmail.com>
2025-12-22 20:33:17 -0300

    concertando o settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
-    'backend.api',
+    'biblioteca',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
